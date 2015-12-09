564dd9bc0f9bd809592a62bbd7ce92c6ea2211ca
qiugang <qiugang@bodecn.net>
2015-12-09 18:16:36 +0800

    add just one view

--- library/src/main/java/me/originqiu/library/GridLayout.java
+++ library/src/main/java/me/originqiu/library/GridLayout.java
@@ -34,65 +34,66 @@ import java.util.ArrayList;
  */
 public class GridLayout extends ViewGroup {
     private static final int DEFAULT_COUNT = 3;
-
+    
     private Paint mGridPaint;
-
+    
     private int mColumnCount;
-
+    
     private int mMaxChildren;
-
+    
     private int childCount;
-
+    
     /**
      * child view click callback
      */
     private ChildViewClick mChildClick;
-
+    
     /**
      * when the view is just one ,set the special bounds
      */
     private boolean isOneView = false;
-
+    
     private int oneViewWith = 120;
-
+    
     private int oneViewHeight = 100;
-
+    
     public GridLayout(Context context) {
         this(context, null);
     }
-
+    
     public GridLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
-
+    
     public GridLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.GridLayout,
-                0,
-                defStyle);
-
+                                                      R.styleable.GridLayout,
+                                                      0,
+                                                      defStyle);
+                                                      
         int strokeWidth = a.getDimensionPixelSize(R.styleable.GridLayout_separatorWidth,
-                0);
+                                                  0);
         int strokeColor = a.getColor(R.styleable.GridLayout_separatorColor,
-                Color.BLACK);
-
-        int viewWidth = a.getDimensionPixelSize(R.styleable.GridLayout_oneViewHeight, 0);
-        int viewHeight = a.getDimensionPixelSize(R.styleable.GridLayout_oneViewHeight, 0);
-
+                                     Color.BLACK);
+                                     
+        int viewWidth = a.getDimensionPixelSize(R.styleable.GridLayout_oneViewHeight,
+                                                0);
+        int viewHeight = a.getDimensionPixelSize(R.styleable.GridLayout_oneViewHeight,
+                                                 0);
+                                                 
         if (viewHeight != 0 && viewWidth != 0) {
             isOneView = true;
             oneViewWith = viewWidth;
             oneViewHeight = viewHeight;
         }
-
-
+        
         mColumnCount = a.getInteger(R.styleable.GridLayout_numColumns,
-                DEFAULT_COUNT);
+                                    DEFAULT_COUNT);
         mMaxChildren = mColumnCount * mColumnCount;
         childCount = mMaxChildren;
         a.recycle();
-
+        
         // init paint
         mGridPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mGridPaint.setStyle(Paint.Style.STROKE);
@@ -100,129 +101,149 @@ public class GridLayout extends ViewGroup {
         mGridPaint.setStrokeWidth(strokeWidth);
         Log.d("init view", "init view");
     }
-
+    
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
+        
         // when there is just one view and you custom the view bounds
         if (childCount == 1 && isOneView) {
+            int widthSpec = MeasureSpec.makeMeasureSpec(oneViewWith,
+                                                        MeasureSpec.EXACTLY);
+            int heightSpec = MeasureSpec.makeMeasureSpec(oneViewHeight,
+                                                         MeasureSpec.EXACTLY);
+            measureChildren(widthSpec, heightSpec);
             setMeasuredDimension(oneViewWith, oneViewHeight);
-        } else {
+        }
+        else {
             int widthSize, heightSize;
             // Get the width based on the measure specs
             widthSize = getDefaultSize(0, widthMeasureSpec);
-
+            
             // Get the height based on measure specs
             heightSize = getDefaultSize(0, heightMeasureSpec);
             int majorDimension = widthSize;
             // Measure all child views
             int blockDimension = majorDimension / mColumnCount;
             int blockSpec = MeasureSpec.makeMeasureSpec(blockDimension,
-                    MeasureSpec.EXACTLY);
+                                                        MeasureSpec.EXACTLY);
             measureChildren(blockSpec, blockSpec);
-
+            
             int singleHeight = majorDimension / mColumnCount;
             setMeasuredDimension(widthSize,
-                    childCount % mColumnCount == 0 ? singleHeight
-                            * (childCount
-                            / mColumnCount)
-                            : singleHeight
-                            * (childCount
-                            / mColumnCount
-                            + 1));
+                                 childCount % mColumnCount == 0 ? singleHeight
+                                                                  * (childCount
+                                                                     / mColumnCount)
+                                                                : singleHeight
+                                                                  * (childCount
+                                                                     / mColumnCount
+                                                                     + 1));
         }
-
+        
         Log.d("view onMeasure", "view onMeasure");
     }
-
+    
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int row, col, left, top;
-        for (int i = 0; i < getChildCount(); i++) {
-            row = i / mColumnCount;
-            col = i % mColumnCount;
-            View child = getChildAt(i);
-            left = col * child.getMeasuredWidth();
-            top = row * child.getMeasuredHeight();
-
-            child.layout(left,
-                    top,
-                    left + child.getMeasuredWidth(),
-                    top + child.getMeasuredHeight());
+        if (childCount > 1) {
+            int row, col, left, top;
+            for (int i = 0; i < getChildCount(); i++) {
+                row = i / mColumnCount;
+                col = i % mColumnCount;
+                View child = getChildAt(i);
+                left = col * child.getMeasuredWidth();
+                top = row * child.getMeasuredHeight();
+                
+                child.layout(left,
+                             top,
+                             left + child.getMeasuredWidth(),
+                             top + child.getMeasuredHeight());
+            }
+        }
+        else if (childCount == 1) {
+            View child = getChildAt(0);
+            child.layout(0,
+                         0,
+                         child.getMeasuredWidth(),
+                         child.getMeasuredHeight());
         }
         Log.d("view onLayout", "view onLayout");
     }
-
+    
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
-        // draw grid separator
-        for (int i = getWidth() / mColumnCount; i < getWidth(); i += (getWidth() / mColumnCount)) {
-            canvas.drawLine(i, 1, i, getHeight(), mGridPaint);
-        }
-        int count = childCount % mColumnCount == 0 ? childCount / mColumnCount
-                : childCount / mColumnCount
-                + 1;
-        for (int i = getHeight() / count; i < getHeight(); i += (getHeight() / count)) {
-            canvas.drawLine(1, i, getWidth(), i, mGridPaint);
+        if (childCount > 1) {
+            // draw grid separator
+            for (int i = getWidth()
+                         / mColumnCount; i < getWidth(); i += (getWidth()
+                                                               / mColumnCount)) {
+                canvas.drawLine(i, 1, i, getHeight(), mGridPaint);
+            }
+            int count = childCount
+                        % mColumnCount == 0 ? childCount / mColumnCount
+                                            : childCount / mColumnCount + 1;
+            for (int i = getHeight() / count; i < getHeight(); i += (getHeight()
+                                                                     / count)) {
+                canvas.drawLine(1, i, getWidth(), i, mGridPaint);
+            }
         }
     }
-
+    
     @Override
     public void addView(View child) {
         if (getChildCount() > mMaxChildren - 1) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
+                                            + mMaxChildren
+                                            + " direct children");
         }
         Log.d("add view", "add view");
         super.addView(child);
     }
-
+    
     @Override
     public void addView(View child, int index) {
         if (getChildCount() > mMaxChildren - 1) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
+                                            + mMaxChildren
+                                            + " direct children");
         }
         Log.d("add view", "add view");
         super.addView(child, index);
     }
-
+    
     @Override
     public void addView(View child, int index, LayoutParams params) {
         if (getChildCount() > mMaxChildren - 1) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
+                                            + mMaxChildren
+                                            + " direct children");
         }
         Log.d("add view", "add view");
         super.addView(child, index, params);
     }
-
+    
     @Override
     public void addView(View child, LayoutParams params) {
         if (getChildCount() > mMaxChildren - 1) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
+                                            + mMaxChildren
+                                            + " direct children");
         }
         Log.d("add view", "add view");
         super.addView(child, params);
     }
-
+    
     @Override
     public void addView(View child, int width, int height) {
         if (getChildCount() > mMaxChildren - 1) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
+                                            + mMaxChildren
+                                            + " direct children");
         }
         Log.d("add view", "add view");
         super.addView(child, width, height);
     }
-
+    
     /**
      * child view click listener
      */
@@ -233,7 +254,7 @@ public class GridLayout extends ViewGroup {
                 mChildClick.onChildClick((Integer) view.getTag());
         }
     };
-
+    
     /**
      * set the view child, hide the other child
      *
@@ -243,28 +264,30 @@ public class GridLayout extends ViewGroup {
         childCount = count;
         if (count > mMaxChildren) {
             throw new IllegalStateException("BoxGridLayout cannot have more than "
-                    + mMaxChildren
-                    + " direct children");
-        } else {
+                                            + mMaxChildren
+                                            + " direct children");
+        }
+        else {
             for (int i = 0; i < mMaxChildren; i++) {
                 if (i < count) {
                     getChildAt(i).setVisibility(VISIBLE);
                     getChildAt(i).setTag(i);
                     getChildAt(i).setOnClickListener(mOnClickListener);
-                } else
+                }
+                else
                     getChildAt(i).setVisibility(GONE);
             }
             invalidate();
         }
     }
-
+    
     /**
      * the interface for child view click
      */
     public interface ChildViewClick {
         void onChildClick(int position);
     }
-
+    
     /**
      * set child view click call back
      *
@@ -273,7 +296,7 @@ public class GridLayout extends ViewGroup {
     public void setChildViewClickCallback(ChildViewClick childViewClickCallback) {
         mChildClick = childViewClickCallback;
     }
-
+    
     /**
      * get all child view in gridlayout
      */
@@ -284,5 +307,5 @@ public class GridLayout extends ViewGroup {
         }
         return childViews;
     }
-
+    
 }
--- sample/src/main/java/me/originqiu/grildlayout/SingleActivity.java
+++ sample/src/main/java/me/originqiu/grildlayout/SingleActivity.java
@@ -3,28 +3,29 @@ package me.originqiu.grildlayout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import me.originqiu.library.GridLayout;
 
 /**
- * GridLayout use as single
- * Created by OriginQiu on 2015/11/27.
+ * GridLayout use as single Created by OriginQiu on 2015/11/27.
  */
 public class SingleActivity extends AppCompatActivity {
     private GridLayout mGridLayout;
-
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
         mGridLayout = (GridLayout) findViewById(R.id.grid_box);
-        mGridLayout.setChildCount(7);
+        mGridLayout.setChildCount(1);
+        
         mGridLayout.setChildViewClickCallback(new GridLayout.ChildViewClick() {
             @Override
             public void onChildClick(int position) {
@@ -32,11 +33,10 @@ public class SingleActivity extends AppCompatActivity {
             }
         });
         ArrayList<View> views = mGridLayout.getAllChildView();
-        for (View view : views) {
-            Glide.with(this).load("https://www.baidu.com/img/baidu_jgylogo3.gif").into((ImageView)(view));
-        }
-        ImageView imageView = (ImageView) views.get(2);
-        imageView.setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
-
+        Log.i("size", views.size()+"");
+        views.get(0).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        Picasso.with(this)
+               .load("https://www.baidu.com/img/baidu_jgylogo3.gif")
+               .into((ImageView) (views.get(0)));
     }
 }
